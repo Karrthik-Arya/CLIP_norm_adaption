ed48aa9a504305bbf95405c2b68f6b420c762ea4
Karrthik Arya <karrthikarya@gmail.com>
2024-10-08 15:38:45 +0530

    val accuracy meter correction

--- train.py
+++ train.py
@@ -219,7 +219,7 @@ for i in range(epochs):
         val_loss_meter.update(loss.item(), img.size(0))
         # Calculate and update validation accuracy
         acc1 = accuracy(output, ans, topk=(1,))
-        val_accuracy_meter.update(acc1[0].item(), img.size(0))
+        val_accuracy_meter.update(acc1[0].item(), ans.size(0))
         # break
     # cross_loss_meter.reset()
     # cross_accuracy_meter.reset()
